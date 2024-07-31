bca33d129e94b06de06f9543f2a184c9da1f4104
abisheik88 <abishekvijayarangan@gmail.com>
2024-07-31 18:46:39 +0530

    Comments added

--- Sum of numbers/sum.c
+++ Sum of numbers/sum.c
@@ -1,10 +1,14 @@
+//This line indicates the standard input and output libraries from c
 #include <stdio.h>
 
+//This is the main function
 int main()
 {
+    //declare three variables with data type integer
     int num1, num2, sum;
 
     printf("Enter the first number: ");
+    //Accepts user input
     scanf("%d", &num1);
     printf("Enter the first number: ");
     scanf("%d", &num2);
